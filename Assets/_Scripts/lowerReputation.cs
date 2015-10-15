76cb09cd07e0596c389ad6651cd41f0da302e91c
Avi Mattenson <Avi@Avis-MacBook-Pro.local>
2015-10-15 13:41:11 -0400

    hooked up time and reputation

---
+++ Assets/_Scripts/lowerReputation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class lowerReputation : MonoBehaviour {
+
+	public GameObject reputationBarGO;
+
+	void OnControllerColliderHit(ControllerColliderHit hit){
+		if(hit.gameObject.tag == "NPC"){
+			Vector2 temp = new Vector2 (reputationBarGO.GetComponent<Image> ().rectTransform.sizeDelta.x - 10, reputationBarGO.GetComponent<Image> ().rectTransform.sizeDelta.y);
+			reputationBarGO.GetComponent<Image> ().rectTransform.sizeDelta = temp;
+			hit.gameObject.tag = "NPC_hit";
+		}
+	}
+}
---
+++ Assets/_Scripts/timeController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class timeController : MonoBehaviour {
+
+	public float min;
+	public float sec;
+
+	public GameObject time;
+
+	// Update is called once per frame
+	void Update () {
+		sec -= Time.deltaTime;
+		if (sec <= 0 && min <= 0) {
+			endGame();
+		}
+		if (sec <= -.5f) {
+			sec = 59;
+			min -= 1;
+		}
+		time.GetComponent<Text> ().text = getTimeString (min) + ":" + getTimeString (sec);
+	}
+
+	string getTimeString(float val){
+		if (val < 9.5f) {
+			return "0" + val.ToString("0");
+		}
+		return val.ToString("0");
+	}
+
+	void endGame(){
+		print ("gameOver");
+	}
+
+}
