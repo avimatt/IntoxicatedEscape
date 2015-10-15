88c21a8bba5ae0a8a669d64cdf4cb834c33b3174
Avi Mattenson <Avi@Avis-MacBook-Pro.local>
2015-10-14 22:36:16 -0400

    added camera re-adjust and limits on how far player can lean

--- Assets/FirstPersonController.cs
+++ Assets/FirstPersonController.cs
@@ -6,12 +6,15 @@ public class FirstPersonController : MonoBehaviour {
 	public float actualMoveSpeed;
 	public GameObject cameraGO;
 
-	float _sideSpeed;
 	CharacterController cc;
+	public bool leaningLeft;
+	public bool leaningRight;
+	public bool correctingAngle;
 
 	// Use this for initialization
 	void Start () {
 		cc = GetComponent<CharacterController> ();
+		correctingAngle = false;
 	}
 	
 	// Update is called once per frame
@@ -19,31 +22,79 @@ public class FirstPersonController : MonoBehaviour {
 
 		//Movement
 		float sideSpeed = Input.GetAxis ( "Horizontal" ) * actualMoveSpeed;
+		Vector3 angles = cameraGO.transform.rotation.eulerAngles;
 
-		//TODO make sure you can't rotate 360 degrees
-		if (sideSpeed > 0) {
-			cameraGO.transform.Rotate (0, 0, -2);
-		} else if (sideSpeed < 0) {
-			cameraGO.transform.Rotate (0, 0, 2);
+		if (sideSpeed > 0 && isLegalRotation (sideSpeed)) { // Lean to the right
+
+			if(correctingAngle){
+				angles[2] = Mathf.Round(angles[2]);
+				cameraGO.transform.rotation = Quaternion.Euler(angles);
+				correctingAngle = false;
+			}
+
+			cameraGO.transform.Rotate (0, 0, -1);
+			angles[2] = cameraGO.transform.rotation.eulerAngles[2];
+
+			leaningRight = angles[2] >= 340 && angles[2] <= 360; 
+			leaningLeft = angles[2] >= 0 && angles[2] <= 20; 
+
+		} else if (sideSpeed < 0 && isLegalRotation (sideSpeed)) { // Lean to the left
+
+			if(correctingAngle){
+				angles[2] = Mathf.Round(angles[2]);
+				cameraGO.transform.rotation = Quaternion.Euler(angles);
+				correctingAngle = false;
+			}
+
+			cameraGO.transform.Rotate (0, 0, 1);
+			angles[2] = cameraGO.transform.rotation.eulerAngles[2];
+
+			leaningLeft = angles[2] >= 0 && angles[2] <= 20; 
+			leaningRight = angles[2] >= 340 && angles[2] <= 360; 
+
+		} else { // When not moving to lean
+
+			if(leaningLeft){
+				correctingAngle = true;
+				angles[2] = angles[2] - (angles[2] * .05f);
+				cameraGO.transform.rotation = Quaternion.Euler(angles);
+				if(cameraGO.transform.rotation.eulerAngles[2] < 0.009){
+					leaningLeft = false;
+				}
+			} 
+			if(leaningRight) {
+				correctingAngle = true;
+				angles[2] = angles[2] + ((360 - angles[2]) * .05f);
+				cameraGO.transform.rotation = Quaternion.Euler(angles);
+
+				if(cameraGO.transform.rotation.eulerAngles[2] < 0.009){
+					leaningRight = false;
+				}
+			}
+
 		}
 
-		_sideSpeed = sideSpeed != 0 ? sideSpeed : _sideSpeed;
-		Vector3 speed = sideSpeed == 0 ? new Vector3 ( -1.5f, 0, _sideSpeed * 10 ) : new Vector3 ( -1.5f, 0, _sideSpeed );
-
+		Vector3 speed = new Vector3 ( -1.5f, 0, sideSpeed );
 		cc.SimpleMove( speed );
 
 	}
 
-	void FixedUpdate () {
-
-		// have the camera shake a bit when tilted (doesn't quite work yet)
-		float shackyCoefficient = 0; 
-		if( cameraGO.transform.rotation[2] < 0 ){
-			shackyCoefficient = cameraGO.transform.rotation[2] + Random.Range(0f, 4f);
-		} else if ( cameraGO.transform.rotation[2] < 0 ) {
-			shackyCoefficient = cameraGO.transform.rotation[2] + Random.Range(-4f, 0f);
+	bool isLegalRotation(float sideSpeed){
+		Vector3 angles = cameraGO.transform.rotation.eulerAngles;
+		if (sideSpeed > 0) {
+			float zAngle = angles[2] - 1 <= 0 ? 360 : angles[2] - 1;
+			if ((zAngle >= 340 && zAngle <= 360) || (zAngle <= 20 && zAngle >= 0)) {
+				return true;
+			}
 		}
-		cameraGO.transform.Rotate (0, 0, shackyCoefficient);
+		if (sideSpeed < 0) {
+			float zAngle = angles[2] + 1 >= 360 ? 0 : angles[2] + 1;
+			if ((zAngle >= 340 && zAngle <= 360) || (zAngle <= 20 && zAngle >= 0)) {
+				return true;
+			}
+		}
+		return false;
 	}
+
 
 }
